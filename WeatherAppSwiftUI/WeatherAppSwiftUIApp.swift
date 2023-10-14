12d5c56c3d3c37d621b6c47af47576df4cfd259f
Tsikhon Bazar <103754328+tihon2005@users.noreply.github.com>
2023-10-14 22:55:08 +0300

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Tikhon Bazar on 14.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
